70bc95f155a5abdb69c265981759ebc519dae13d
AttilaTurkoz <42498753+attila5287@users.noreply.github.com>
2025-06-30 05:32:08 -0600

    area route: agri-field layer + user extrusion layer completed>> now: elevated line layer

---
+++ src/config/DefaultUserInput.jsx
@@ -0,0 +1,5 @@
+export const DefaultUserInput = {
+  height: 10,
+  angleCourse: 4,
+  stepCount: 8,
+};
---
+++ src/config/ExtrusionPaint.jsx
@@ -0,0 +1,7 @@
+export const ExtrusionPaint = {
+  "fill-extrusion-color": "SkyBlue",
+  "fill-extrusion-opacity": 0.5,
+  "fill-extrusion-emissive-strength": 0.3,
+  "fill-extrusion-flood-light-color": "RoyalBlue",
+  "fill-extrusion-flood-light-ground-radius": 1.5,
+};
